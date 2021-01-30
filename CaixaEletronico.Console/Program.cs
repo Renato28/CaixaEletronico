b54cb93d56dba6c2e8155d8473cc5693f56654ca
Renato Nobrega <renatonobrega87@gmail.com>
2021-01-30 11:10:26 -0300

    projeto atualizado

--- CaixaEletronico.Console/Program.cs
+++ CaixaEletronico.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CaixaEletronico.Dominio.Entidades;
+using System;
 
 namespace CaixaEletronico.ConsoleApp
 {
@@ -6,7 +7,18 @@ namespace CaixaEletronico.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Iniciando o Caixa Eletronico");
+
+            var cliente = new Cliente();
+            cliente.Nome = "Renato Nóbrega";
+            cliente.DataNascimento = new DateTime(1987, 17, 12);
+            cliente.Cpf = "06696560467";
+
+            var conta = new Conta();
+            conta.Agencia = "123456"
+            conta.Numero = "56789";
+            conta.Saldo = 50000;
+            conta.Ativa = true;
 
             Console.ReadLine();
         }
